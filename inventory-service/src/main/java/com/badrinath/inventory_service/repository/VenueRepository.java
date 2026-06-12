25321e8ea0d11d7311a7a723eb612fc5162ec367
Badrinath Rudroju <rudrojubadrinath27@gmail.com>
2026-06-12 22:38:27 +0530

    Revert "to push by creating a new branch"
    This reverts commit bf5e8f7668a0d9982317e0a2a397a80990718ab9.

--- inventory-service/src/main/java/com/badrinath/inventory_service/repository/VenueRepository.java
+++ inventory-service/src/main/java/com/badrinath/inventory_service/repository/VenueRepository.java
@@ -2,6 +2,7 @@ package com.badrinath.inventory_service.repository;
 
 import com.badrinath.inventory_service.entity.Venue;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
