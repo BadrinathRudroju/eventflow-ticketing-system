704ce45a999e68b7770d87053c15649a7e9f9eb1
Badrinath Rudroju <rudrojubadrinath27@gmail.com>
2026-06-20 01:41:06 +0530

    Fix Flyway migration + externalize DB credentials

--- inventory-service/src/main/java/com/badrinath/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/badrinath/inventory_service/InventoryServiceApplication.java
@@ -1,5 +1,6 @@
 package com.badrinath.inventory_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("C:/Projects/eventflow-ticketing-system/inventory-service")
+				.ignoreIfMissing()
+				.load();
+
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME", ""));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD", ""));
+
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
-
 }
